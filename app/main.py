524a063a533e03fe1a6fe8b8b3881d20cc693a4e
Łukasz Szenkiel <lukasz.szenkiel@studenci.collegiumwitelona.pl>
2025-04-15 00:24:41 +0200

    tex

--- app/main.py
+++ app/main.py
@@ -3,42 +3,58 @@ from expense import Expense
 from calculation import Calculation
 from output import *
 
-print("Program rozlicza wydatki grupowe i na koniec podaje, kto komu ile ma zwrócić.")
-names = input("\nPodaj imiona (unikatowe) uczesników rozliczenia (rozdzielone przecinkami):\n")
-team = []
-for name in names.split(","):
-    team.append(Person(name.strip()))
-expenses = []
-choice = 1
+useTestData = True
+if not useTestData:
+    print("Program rozlicza wydatki grupowe i na koniec podaje, kto komu ile ma zwrócić.")
+    names = input("\nPodaj imiona (unikatowe) uczesników rozliczenia (rozdzielone przecinkami):\n")
+    team = []
+    for name in names.split(","):
+        team.append(Person(name.strip()))
+    expenses = []
+    choice = 1
 
-def add_expense():
-    expense_name = input("Podaj nazwę wydatku:\n")
-    print("Kto zapłacił(podaj numer odpowiedniej osoby)")
+    def add_expense():
+        expense_name = input("Podaj nazwę wydatku:\n")
+        print("Kto zapłacił(podaj numer odpowiedniej osoby)")
+        for person in team:
+            print(f"{team.index(person) + 1} - {person.name}")
+        payer = int(input()) - 1
+        amount = float(input(f"Jaką kwotę zapłacił {team[payer].name}?\n"))
+        print("Wybierz, kto korzystał z tego wydatku: (podaj numery rozdzielone przecinkami)")
+        for person in team:
+            print(f"{team.index(person) + 1} - {person.name}")
+        print(f"{len(team) + 1} - wszyscy")
+        beneficiaries_numbers = list(map(int, input("Podaj liczby oddzielone przecinkami: ").split(",")))
+        beneficiaries = []
+        if beneficiaries_numbers[0] == len(team) + 1:
+            beneficiaries = team
+        else:
+            for person in beneficiaries_numbers:
+                beneficiaries.append(team[person - 1])
+        expenses.append(Expense(expense_name, team[payer], amount, beneficiaries))
+
+    while choice != 2:
+        add_expense()
+        choice = int(input("\n1 - dodaj wydatek\n2 - rozliczenie"))
+
     for person in team:
-        print(f"{team.index(person) + 1} - {person.name}")
-    payer = int(input()) - 1
-    amount = float(input(f"Jaką kwotę zapłacił {team[payer].name}?\n"))
-    print("Wybierz, kto korzystał z tego wydatku: (podaj numery rozdzielone przecinkami)")
-    for person in team:
-        print(f"{team.index(person) + 1} - {person.name}")
-    print(f"{len(team) + 1} - wszyscy")
-    beneficiaries_numbers = list(map(int, input("Podaj liczby oddzielone przecinkami: ").split(",")))
-    beneficiaries = []
-    if beneficiaries_numbers[0] == len(team) + 1:
-        beneficiaries = team
-    else:
-        for person in beneficiaries_numbers:
-            beneficiaries.append(team[person - 1])
-    expenses.append(Expense(expense_name, team[payer], amount, beneficiaries))
-
-while choice != 2:
-    add_expense()
-    choice = int(input("\n1 - dodaj wydatek\n2 - rozliczenie"))
-
-for person in team:
-    print(person)
+        print(person)
+else:
+    team = []
+    expenses = []
+    team.append(Person("Maciek"))
+    team.append(Person("Agata"))
+    team.append(Person("Wojtek"))
+    team.append(Person("Milena"))
+    team.append(Person("Eugeniusz"))
+    team.append(Person("Andrzej"))
+    expenses.append(Expense("Zakupy spożywcze",payer=team[0], amount=120.00, beneficiaries=[team[0], team[1], team[2]]))
+    expenses.append(Expense("Bilety do kina", payer=team[1], amount=90.00, beneficiaries=[team[0], team[1], team[2], team[3]]))
+    expenses.append(Expense("Pizza", payer=team[2], amount=80.00, beneficiaries=team))
+    expenses.append(Expense("Taxi", payer=team[4], amount=60.00, beneficiaries=[team[4], team[5]]))
+    expenses.append(Expense("Hotel", payer=team[3], amount=200.00, beneficiaries=team))
+    expenses.append(Expense("Dyskoteka", payer=team[1], amount=70.00, beneficiaries=[team[1], team[3]]))
 
 refunds = Calculation(team)
-generate_diagram(refunds)
-generate_md(team, expenses, refunds)
+generate_output(refunds, team, expenses)
 print(refunds)
--- app/output.py
+++ app/output.py
@@ -1,22 +1,31 @@
 from calculation import *
 from accounting import *
 from expense import *
+from datetime import datetime
+from plantuml import PlantUML
+import subprocess
 import pypandoc
 import os
 
 os.makedirs("out", exist_ok=True)
 
-def generate_diagram(calculation):
-    result = "@startuml\n"
-    for accounting in calculation.accountings:
-        result += f"{accounting.refund_giver.name} --> {accounting.refund_receiver.name} : {accounting.amount}\n"
+def generate_output(refunds: Calculation, team: List[Person], expenses: List[Expense]):
+    #Generowanie diagramu w plantUML
+    result = "@startuml\nleft to right direction\n"
+    for person in team:
+        result += f"object {person.name} {{\nNależność : {person.debt:.2f}zł\nWydatki: {person.expenses:.2f}zł\n}}\n"
+    for accounting in refunds.accountings:
+        result += f"{accounting.refund_giver.name} --> {accounting.refund_receiver.name} : {accounting.amount:.2f}zł\n"
     result += "@enduml\n"
-    with open("out/diagram_rozliczenie.puml", "w", encoding="utf-8") as diagram_file:
+    puml_file_path = "out/diagram_rozliczenie.puml"
+    with open(puml_file_path, "w", encoding="utf-8") as diagram_file:
         diagram_file.write(result)
+    plantuml = PlantUML(url='http://www.plantuml.com/plantuml/img/')
+    plantuml.processes_file(puml_file_path)
 
-def generate_md(team: List[Person], expenses: List[Expense], refunds: Calculation):
+
+    #Generowanie raportu w Markdown z eksportem do PDF
     result = "# Raport rozliczeniowy wydatków grupowych\n\n"
-
     result += "\n### Uczestnicy rozliczenia\n"
     for person in team:
         result += f"- {person.name}\n"
@@ -30,14 +39,20 @@ def generate_md(team: List[Person], expenses: List[Expense], refunds: Calculatio
         result += f"**Beneficjenci wydatku:** {', '.join([person.name for person in expense.beneficiaries])}  \n"
         result += f"**Należność na głowę:** {expense.debt:.2f} zł\n\n"
 
-    result += "---\n### Indywidualny koszt wyprawy\n|Osoba|Należność|\n|---|---|\n"
+    result += "---\n### Indywidualny koszt\n|Osoba|Należność|\n|---|---|\n"
     for person in team:
         result += f"|{person.name}|{person.debt:.2f} zł|\n"
 
     result += "---\n### Rozliczenie"
     for accounting in refunds.accountings:
         result += f"\n\n{accounting.refund_giver.name} --- {accounting.amount:.2f} zł ---> {accounting.refund_receiver.name}\n"
+    result += '\n\n---\n### Diagram rozliczenia\n'
+    result += '<p align="center"><img src="diagram_rozliczenie.png" width="600"/></p>\n'
+    result += f"\n*Raport wygenerowano: {datetime.now().strftime('%d-%m-%Y %H-%M-%S')}*"
+    subprocess.run(["markdown-pdf", "out/raport.md", "-o", "out/raport.pdf"])
 
+    """
     with open("out/raport.md", "w", encoding="utf-8") as raport:
         raport.write(result)
     pypandoc.convert_file("out/raport.md", to="pdf", format="md", outputfile="out/raport.pdf", extra_args=["-t", "latex"])
+    """
